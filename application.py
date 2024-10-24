1a0b3ce93f58c251ee24bd5708d989b5131a5a95
ria147 <62183694+ria147@users.noreply.github.com>
2024-10-24 13:05:17 -0400

    Update application.py

--- application.py
+++ application.py
@@ -6,6 +6,11 @@ import pickle
 # Initialize the Flask application
 application = Flask(__name__) 
 
+# Index
+@application.route('/')
+def home():
+    return 'Welcome to Fake News Classifier!'
+
 # Model loading
 loaded_model = None
 with open('basic_classifier.pkl', 'rb') as fid:
@@ -16,16 +21,18 @@ with open('count_vectorizer.pkl', 'rb') as vd:
     vectorizer = pickle.load(vd)
 
 # Prediction
-prediction = loaded_model.predict(vectorizer.transform(['This is fake news']))[0]
+@application.route('/predict')
+def predict():
+    prediction = loaded_model.predict(vectorizer.transform(['This is fake news']))[0]
 
-# Output will be 'FAKE' if fake, 'REAL' if real
-if prediction == 1:
-    print("FAKE")
-else:
-    print("REAL")
-    
+    # Output will be 'FAKE' if fake, 'REAL' if real
+    if prediction == 1:
+        print("FAKE")
+    else:
+        print("REAL")
+
 # Start the Flask app
 if __name__ == '__main__':
     # In this simple example, there is no Flask route to handle requests
     # Just add this line to follow the structure in Figure 12
-    application.run()
+    application.run(host="0.0.0.0", port=8080)
